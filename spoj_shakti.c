48c2649e17d04b053012a32de0a7ca7d9b74cf9f
Aditya Pimparkar <adityapimparkar22@gmail.com>
2016-08-23 23:20:24 +0530

    Add files via upload

---
+++ spoj_shakti.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<stdlib.h>
+int main(){
+	int t;
+	scanf("%d",&t);
+	int i;
+	for(i=0;i<t;i++){
+	int n;
+	scanf("%d",&n);
+	if(n%2==0){
+	printf("Thankyou Shaktiman\n");
+	
+	}
+	else{
+	printf("Sorry Shaktiman\n");
+	}
+}
+return 0;
+}
